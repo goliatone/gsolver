cb3f89c6fd4b7336a49e5ac5fdb961fa852af974
goliatone <burgosemi@gmail.com>
2016-04-23 18:15:53 -0400

    adding example

---
+++ example/test.js
@@ -0,0 +1,46 @@
+var Solver = require('..');
+
+var s = new Solver();
+
+var context = {
+    name:'Pepe',
+    hola: 'Hello',
+    user:{
+        name: 'Peperone',
+        address:{
+            city:'New York'
+        }
+    },
+    wtf: '${name}, how are you?'
+};
+
+var solved = s.solve({
+    message:'${name}, how are you?',
+    greet: '${hola} ${user.name}!!! Still living in ${user.address.city}?',
+    testing: {
+       nested: {
+           templates: '${user.address.city}'
+       }
+    }
+}, context);
+console.log('***********')
+console.log(solved.greet)
+console.log(solved.message)
+console.log(solved.testing)
+
+s.template.openTag = '@{';
+solved = s.solve({
+    message:'@{name} was here!',
+    greet: '@{hola}, @{user.address.city}?'
+}, context);
+
+console.log('|||||||||||||||||')
+console.log(solved.greet)
+console.log(solved.message)
+
+s.template.openTag = '${';
+solved = s.solve(context);
+console.log('///////////////////')
+console.log(solved.name)
+console.log(solved.hola)
+console.log(solved.wtf)

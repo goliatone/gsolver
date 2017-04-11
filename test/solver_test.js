a00e84e1e0cd6cdb774dc3311c28f25d7a260a69
goliatone <burgosemi@gmail.com>
2017-04-10 21:19:32 -0400

    update test

--- test/solver_test.js
+++ test/solver_test.js
@@ -16,5 +16,100 @@ describe('Solver', function(){
         it('should provide a DEFAULTS object', function(){
             assert.isObject(Solver.DEFAULTS);
         });
+
+        it('should not modify string templates if not found in the context', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                name: '${name}'
+            }, {});
+            assert.equal(solved.name, '${name}');
+        });
+
+        it('should replace tags with its value in the context object', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                name: '${name}'
+            }, {
+                name: 'Peperone'
+            });
+            assert.equal(solved.name, 'Peperone');
+        });
+
+        it('should replace multiple tags with its value in the context object', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                fullName: '${firstName} ${lastName}'
+            }, {
+                firstName: 'Pepe',
+                lastName: 'Rone'
+            });
+            assert.equal(solved.fullName, 'Pepe Rone');
+        });
+
+        it('should replace nested elements', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                city: '${user.address.city}'
+            }, {
+                user: {
+                    address: {
+                        city: 'New York'
+                    }
+                }
+            });
+            assert.equal(solved.city, 'New York');
+        });
+
+        it('should take a single argument and solve template tags in the context itself', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                name: 'Peperone',
+                greeting: '${name} was here'
+            });
+            assert.equal(solved.greeting, 'Peperone was here');
+        });
+
+        it('should resolve object references', function(){
+            var solver = new Solver();
+            var context = {
+                user: {
+                    name: 'Peperone',
+                    address: {
+                        city:'New York'
+                    }
+                }
+            };
+
+            var solved = solver.solve({
+                reference: '@{user.address}'
+            }, context);
+            assert.deepEqual(solved.reference, context.user.address);
+        });
+
+        it('should support custom tags', function(){
+            var solver = new Solver({
+                openTag: '<%',
+                closeTag: '%>'
+            });
+            var solved = solver.solve({
+                fullName: '<%firstName%> <%lastName%>'
+            }, {
+                firstName: 'Pepe',
+                lastName: 'Rone'
+            });
+            assert.equal(solved.fullName, 'Pepe Rone');
+        });
+
+        it('should guard against infinity recursion if a tempalte is not defined', function(){
+            var solver = new Solver();
+            var solved = solver.solve({
+                name: '${user.address.city}'
+            }, {
+                user: {
+                    addres: {}
+                }
+            });
+            assert.equal(solved.name, '${user.address.city}');
+        });
     });
 });
